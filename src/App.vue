ea00b2249400931ef1f26dcc5413d9043c1024d6
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-03-04 20:23:25 +0100

    todo list in vue

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,65 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+
+const header = ref("My Todos");
+const todos = ref([]);
+// const reversedTodos
+const newTodo = ref("");
+const highPriority = ref(false);
+const editing = ref(false);
+const addTodo = () => {
+  todos.value.unshift({
+    id: todos.value.length + 1,
+    label: newTodo.value,
+    highPrio: highPriority.value,
+    completed: false,
+  });
+  highPriority.value = false;
+  newTodo.value = "";
+};
+const edit = (e) => {
+  editing.value = e;
+};
+const toggleComplete = (item) => {
+  item.completed = !item.completed;
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="header">
+    <h1>{{ header }}</h1>
+    <button
+      v-if="editing"
+      class="btn btn-cancel btn-header"
+      @click="edit(false)"
+    >
+      Cancel
+    </button>
+    <button v-else class="btn btn-primary btn-header" @click="edit(true)">
+      Add Items
+    </button>
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <br />
+  <form class="add-item-form" v-if="editing" @submit.prevent="addTodo()">
+    <input type="text" placeholder="What do you need to do" v-model="newTodo" />
+    <label>
+      <input type="checkbox" v-model="highPriority" /> High Priority
+    </label>
+    <button class="btn btn-primary" :disabled="newTodo.length < 5">
+      Add Todo
+    </button>
+  </form>
+  <br />
+  <ul>
+    <li
+      v-for="todo in todos"
+      :class="{ strikeout: todo.completed, priority: todo.highPrio }"
+      :keys="todo.id"
+      @click="toggleComplete(todo)"
+    >
+      {{ todo.label }}
+    </li>
+  </ul>
+  <p v-if="!todos.length">Nothing to do!</p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
